2cd3239f33300d81a5696f24fceac44b55d5d981
Sura Alemu <111273870+suraphelalemu@users.noreply.github.com>
2025-10-31 16:36:00 +0300

    Update auth.js adding admin register

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, verify } from "../controllers/authController.js";
+import { login, verify,createAdmin } from "../controllers/authController.js";
 import authMiddleware from "../middleware/authMiddlware.js";
 import { logout } from "../controllers/Logout.js";
 
@@ -8,6 +8,7 @@ const router = express.Router();
 router.post("/login", login);
 router.get("/verify", authMiddleware, verify);
 router.post("/logout", logout);
+router.post("/create-admin",createAdmin);
 export default router;
 
 
